fc7d9b2ab39fe697174174928f16b153f79164fe
Nikolay <nikolay.ratushnyak03@gmail.com>
2023-02-17 02:28:21 +0700

    only buttons

--- app/src/main/java/com/example/drumpadmachinebeats/MainActivity.java
+++ app/src/main/java/com/example/drumpadmachinebeats/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.drumpadmachinebeats;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private SoundPool soundBank;
+    private int sound1;
+    private int sound2;
+    private int sound3;
+    private int sound4;
+    private int sound5;
+    private int sound6;
+    private int sound7;
+    private int sound8;
+    private int sound9;
+    private int sound10;
+    private int sound11;
+    private int sound12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playsample1 (View v){
+
+    }
+    public void playsample2 (View v){
+
+    }
+    public void playsample3 (View v){
+
+    }
+    public void playsample4 (View v){
+
+    }
+    public void playsample5 (View v){
+
+    }
+    public void playsample6 (View v){
+
+    }
+    public void playsample7 (View v){
+
+    }
+    public void playsample8 (View v){
+
+    }
+    public void playsample9 (View v){
+
+    }
+    public void playsample10 (View v){
+
+    }
+    public void playsample11 (View v){
+
+    }
+    public void playsample12 (View v){
+
     }
 }
